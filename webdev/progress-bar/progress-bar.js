8cce65ef4599b655b79873bf100d36833170c754
mriioos <114981304+mriioos@users.noreply.github.com>
2024-09-01 11:31:15 +0100

    .

---
+++ webdev/progress-bar/progress-bar.js
@@ -0,0 +1,7 @@
+// Progress bar code
+function updateProgressBar(progressEl){
+    // Calc the new width %
+
+    // Set the new width to the progress bar progress element
+    progressEl.style.width = `${newWidth}%`;
+}
